58dd8901b5383f85bb025431b988bcf61ce78b1b
Ahmed Galal <ahmedgalal007@hotmail.com>
2023-11-15 06:04:27 +0200

    Before Adding SASS

---
+++ SSBlazorFileManager.ClassLibrary/Components/TreeView/TreeNodeDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSBlazorFileManager.ClassLibrary.Components.TreeView;
+public class TreeNodeDto
+{
+    public string Title { get; set; }
+    public string Icon { get; set; }
+
+    public List<TreeNodeItem> Items { get; set; } = new List<TreeNodeItem>();
+    public List<TreeNodeDto> Nodes { get; set; } = new List<TreeNodeDto>();
+
+}
